1193db1aad6ddda0ba6f470bd5f41001af18d1b9
Jessica Champion <Jess@Jess-Mac.local>
2015-07-25 14:53:48 +1200

    Initial scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jessMakesThingsApp
+ * @description
+ * # jessMakesThingsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jessMakesThingsApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
